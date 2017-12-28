db59b88aad4b0aa0b96abde861849bbf4ad74bb7
SL0TR <yomaimsj@gmail.com>
2017-12-28 23:03:24 +0600

    new global vue object reactive computed property added

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 new Vue({
 	el: '#app',
   data: {
-    counter: 0
+    counter: 0,
+    secondCounter: 0
+  },
+  computed: {
+    output: function() {
+      return this.counter > 5 ? "Greater 5" : "Smaller 5";
+    }
   },
   methods: {
-    increase: function(){
-      this.counter++;
-      this.result = this.counter > 5 ? "Greater 5" : "Smaller 5"
+    result: function() {
+      return this.counter > 5 ? "Greater 5" : "Smaller 5";
     }
   }
 
